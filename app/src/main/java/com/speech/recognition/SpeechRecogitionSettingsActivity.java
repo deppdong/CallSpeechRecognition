93f35e2023cb2aa097e0f021b61019f30147990d
董书建 <dongshujian@meizu.com>
2018-10-26 15:47:20 +0800

    项目初始化

---
+++ app/src/main/java/com/speech/recognition/SpeechRecogitionSettingsActivity.java
@@ -0,0 +1,13 @@
+package com.speech.recognition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpeechRecogitionSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speech_recogition_settings);
+    }
+}
